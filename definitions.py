bf8d2806d425850fe9eee182421add956b31e986
jw <cjw0515@naver.com>
2019-09-07 23:13:05 +0900

    db명 constant추가

--- definitions.py
+++ definitions.py
@@ -4,6 +4,10 @@ PROJECT_ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
 STATIC_PATH = os.path.join(PROJECT_ROOT_PATH, r"dist\static")
 TEMPLATE_FOLDER = os.path.join(PROJECT_ROOT_PATH, r"dist")
 
+"""
+    데이터베이스
+"""
+INSTI = "INSTI"
+CJW0515_DB = "CJW0515_DB"
+BO = "BO"
 
-if __name__ == "__main__":
-    print(PROJECT_ROOT_PATH)
