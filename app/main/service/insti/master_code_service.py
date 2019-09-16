78d86d96f011b477600687e67e376ea4c19e0c62
DESKTOP-4PMUB76\최종원 <cjw0515@naver.com>
2019-09-16 20:49:45 +0900

    subquery 로 자식코드 가져오는 로직 추가

--- app/main/service/insti/master_code_service.py
+++ app/main/service/insti/master_code_service.py
@@ -1,5 +1,4 @@
 from app.main import db
-
 from app.main.model.insti.models import CodeMast
 
 
@@ -8,4 +7,34 @@ def get_a_codes(code):
 
 
 def get_codes(parent_code, depth):
-    return CodeMast.query.filter_by(parent_code_no=parent_code, depth=depth).all()
+    """
+    SELECT a.* 	  
+          , b.cnt
+        FROM code_mast AS a
+      left JOIN (
+          SELECT parent_code_no
+                , COUNT(*) AS cnt
+            FROM code_mast
+          GROUP BY parent_code_no  
+      ) AS b ON b.parent_code_no = a.code_no   
+      WHERE a.parent_code_no = 10
+    """
+    stmt = db.session\
+        .query(CodeMast.parent_code_no, db.func.count('*').label('cnt')).\
+        group_by(CodeMast.parent_code_no)\
+        .subquery()
+
+    result = db.session.query(CodeMast, stmt.c.cnt)\
+        .filter(CodeMast.parent_code_no == parent_code, CodeMast.depth == depth)\
+        .outerjoin(stmt, CodeMast.code_no == stmt.c.parent_code_no).all()
+
+    tmp_list = []
+    for obj in result:
+        tmp_list.append(obj[0])
+        # print(obj[0].parent_code_no)
+
+    # [(< CodeMast 100 >, 2), (< CodeMast 101 >, 2), (< CodeMast 102 >, 1), (< CodeMast 106 >, 1), (< CodeMast 108 >, 1),
+    #  (< CodeMast 110 >, 1)]
+
+
+    return tmp_list
