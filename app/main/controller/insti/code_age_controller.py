c356899c79fec86025598a2081fc8c6ca6f4bf82
DESKTOP-4PMUB76\최종원 <cjw0515@naver.com>
2019-09-18 20:13:44 +0900

    페이징 처리

--- app/main/controller/insti/code_age_controller.py
+++ app/main/controller/insti/code_age_controller.py
@@ -4,12 +4,20 @@ from app.main.util.decorator import token_required
 from app.main.util.insti.code_age_dto import CodeAgeDto
 from app.main.service.insti.age_code_service import get_codes
 
+
 api = CodeAgeDto.api
 ageCode = CodeAgeDto.code_age
+
 
 @api.route('/')
 class AgeCodeList(Resource):
     @api.doc('ageCode data')
     @api.marshal_list_with(ageCode, envelope='data')
     def get(self):
-        return get_codes()
+        page = int(request.args.get('page', 1))
+        per_page = int(request.args.get('per_page', 10))
+
+        # result = get_codes(page=page, per_page=per_page)
+        # print(result)
+
+        return get_codes(page=page, per_page=per_page)
