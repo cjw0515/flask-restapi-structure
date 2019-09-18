b7e864fb0a6e090ba8ecb99f502da3bcb95c079f
DESKTOP-4PMUB76\최종원 <cjw0515@naver.com>
2019-09-18 20:15:04 +0900

    별칭 사용

--- app/main/util/insti/code_age_dto.py
+++ app/main/util/insti/code_age_dto.py
@@ -4,8 +4,8 @@ class CodeAgeDto:
     api = Namespace('codeAge', description='code_age')
 
     code_age = api.model('code_age', {
-        'age_no': fields.Integer(description='나이'),
-        'gbn': fields.String(description='구분. C=그룹. A=별칭'),
-        'age_name': fields.String(description='나이 명칭'),
-        'use_yn': fields.String(description='사용여부')
+        'age': fields.Integer(description='나이', attribute='age_no'),
+        'gbn': fields.String(description='구분. C=그룹. A=별칭', attribute='gbn'),
+        'ageName': fields.String(description='나이 명칭', attribute='age_name'),
+        'status': fields.String(description='사용여부', attribute='use_yn'),
     })
