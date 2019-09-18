1faa4a4b7e31f9a29d4a32172e2cebb8f6fb62ab
DESKTOP-4PMUB76\최종원 <cjw0515@naver.com>
2019-09-18 20:14:19 +0900

    마스터코드 update로직

--- app/main/controller/insti/master_code_controller.py
+++ app/main/controller/insti/master_code_controller.py
@@ -2,22 +2,29 @@ from flask import request
 from flask_restplus import Resource, marshal, fields
 from app.main.util.decorator import token_required
 from app.main.util.insti.master_code_dto import MasterCodeDto
-from app.main.service.insti.master_code_service import get_codes, get_a_codes
+from app.main.service.insti.master_code_service import get_codes, get_a_codes, update_code
 
 api = MasterCodeDto.api
-masterCode = MasterCodeDto.master_code
-
+master_code = MasterCodeDto.master_code
+master_code_mod = MasterCodeDto.master_code_mod
 
 @api.route('/<code_number>')
 @api.param('code_number', '키 코드')
 @api.response(404, 'code not found.')
 class MasterCodeList(Resource):
-    @api.doc('MasterCodeList data')
-    @api.marshal_list_with(masterCode)
+    @api.doc('마스터 코드 get')
+    @api.marshal_list_with(master_code)
     def get(self, code_number):
         result = get_a_codes(code_number)
         return result
 
+    # @token_required
+    @api.doc('코드정보 수정')
+    @api.response(201, '수정 성공.')
+    @api.expect(master_code_mod)
+    def put(self, code_number):
+        data = request.json
+        return update_code(code_number, data)
 
 @api.route('/childCodes/<parent_code>/depth/<depth>', endpoint='childCodes')
 @api.param('parent_code', '부모 코드')
@@ -25,11 +32,12 @@ class MasterCodeList(Resource):
 @api.response(404, 'code not found.')
 class MasterCodeList2(Resource):
     @api.doc('MasterCodeList data')
-    @api.marshal_list_with(masterCode)
     def get(self, parent_code, depth):
         res = get_codes(parent_code, depth)
-        print(res)
-        data = {'a': 100, 'b': 'foo', 'c': None}
-        mfields = {'a': fields.Raw, 'c': fields.Raw, 'd': fields.Raw}
-        print(marshal(res, masterCode))
-        return res
+        tmp_res = []
+        for obj in res:
+            tmp_obj = marshal(obj[0], master_code)
+            tmp_obj['childCnt'] = obj[1] or 0
+            tmp_res.append(tmp_obj)
+
+        return tmp_res
