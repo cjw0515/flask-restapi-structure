9bc2903719f6dc2e393ec4beaf4f739bc0d453ab
DESKTOP-4PMUB76\최종원 <cjw0515@naver.com>
2019-09-18 20:15:19 +0900

    dto추가

--- app/main/util/insti/master_code_dto.py
+++ app/main/util/insti/master_code_dto.py
@@ -8,9 +8,14 @@ class MasterCodeDto:
         'codeNo': fields.Integer(discription='코드', attribute='code_no'),
         'parentCodeNo': fields.Integer(discription='부모 코드', attribute='parent_code_no'),
         'codeName': fields.String(discription='코드 명', attribute='code_name'),
-        'status': fields.Integer(discription='사용 여부', attribute='use_yn'),
+        'status': fields.String(discription='사용 여부', attribute='use_yn'),
         'depth': fields.Integer(discription='depth', attribute='depth'),
         'regId': fields.String(discription='생성자', attribute='reg_id'),
         'regDate': fields.String(discription='생성 일자', attribute='reg_date'),
         'childCnt': fields.Integer(discription='자식 수', attribute='anon_1_cnt'),
+    })
+
+    master_code_mod = api.model('master_code', {
+        'codeName': fields.String(discription='코드 명', attribute='code_name'),
+        'status': fields.Integer(discription='사용 여부', attribute='use_yn'),
     })
