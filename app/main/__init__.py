baa61040cc67ee64dff31f695a0211b6db37d728
jay-bucket_ <cjw1526@gmail.com>
2020-03-30 23:09:39 +0900

    디폴트

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -19,12 +19,11 @@ def create_app(config_name):
                 static_folder=STATIC_PATH,
                 template_folder=TEMPLATE_FOLDER)
     app.config.from_object(config_by_name[config_name])
-    app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_size': 5, 'max_overflow': 10, 'pool_recycle': -1}
     db.init_app(app)
 
     """
-    This helper function wraps the eponymous method of Bcrypt. 
-    It is intended to be used as a helper function at the expense of the configuration variable provided 
+    This helper function wraps the eponymous method of Bcrypt.
+    It is intended to be used as a helper function at the expense of the configuration variable provided
     when passing back the app object.
     """
     flask_bcrypt.init_app(app)
--- app/main/service/insti/academy_service.py
+++ app/main/service/insti/academy_service.py
@@ -26,7 +26,8 @@ def get_academies():
         if request.args.get('queryType') == 'instiNumber':
             tmp_filters.append(Insti.insti_no == query)
         elif request.args.get('queryType') == 'instiName':
-            tmp_filters.append(Insti.insti_name.like('%{query}%'.format(query=query)))
+            tmp_filters.append(Insti.insti_name.like(
+                '%{query}%'.format(query=query)))
 
     if is_check == '1':
         tmp_filters.append(Insti.confirm_yn == 1)
@@ -36,6 +37,7 @@ def get_academies():
     filters = tuple(tmp_filters)
     page_obj = db.session.query(Insti).filter(*filters).order_by(Insti.insti_no.desc()).paginate(page=page,
                                                                                                  per_page=per_page)
+    db.session.commit()
 
     return {'list': page_obj.items, 'total': page_obj.total, 'perPage': page_obj.per_page}
 
@@ -51,6 +53,8 @@ def get_a_academy_info(insti_no):
         tmp_obj['disp'] = o[1]
         details.append(tmp_obj)
 
+    db.session.commit()
+
     return {
         'insti': insti,
         'details': details,
@@ -62,6 +66,8 @@ def get_a_academy(insti_no):
     result = db.session.query(Insti) \
         .filter(Insti.insti_no == insti_no) \
         .first()
+
+    db.session.commit()
 
     return result
 
@@ -82,9 +88,10 @@ def get_details(insti_no):
 
     result = db.session.query(InstiDetail, stmt.c.code_name) \
         .filter(InstiDetail.insti_no == insti_no) \
-        .outerjoin(stmt, and_(InstiDetail.code_no == stmt.c.code_no
-                              , InstiDetail.gbn.in_(SUBJECT_CODE))) \
+        .outerjoin(stmt, and_(InstiDetail.code_no == stmt.c.code_no, InstiDetail.gbn.in_(SUBJECT_CODE))) \
         .all()
+
+    db.session.commit()
 
     # for o in result:
     #     print(o)
@@ -96,6 +103,8 @@ def get_additions(insti_no):
     result = db.session.query(InstiAddition) \
         .filter(InstiAddition.insti_no == insti_no) \
         .all()
+
+    db.session.commit()
 
     return result
 
@@ -142,13 +151,16 @@ def insert_academy(data):
     )
     save_changes(new_insti)
     new_insti_key = new_insti.insti_no
-    if 'detailData' in data: chk_detail_data(data['detailData'], new_insti_key)
-    if 'additionData' in data: chk_addition_data(data['additionData'], new_insti_key)
+    if 'detailData' in data:
+        chk_detail_data(data['detailData'], new_insti_key)
+    if 'additionData' in data:
+        chk_addition_data(data['additionData'], new_insti_key)
 
     response_object = {
         'status': 'success',
         'message': 'Successfully registered.'
     }
+    db.session.commit()
     return response_object, 201
 
 
@@ -157,22 +169,29 @@ def update_academy(key, data):
     if not insti_key:
         return
 
-    if 'insti' in data: update_insti(data['insti'], insti_key)
-    if 'detailData' in data: chk_detail_data(data['detailData'], insti_key)
-    if 'additionData' in data: chk_addition_data(data['additionData'], insti_key)
+    if 'insti' in data:
+        update_insti(data['insti'], insti_key)
+    if 'detailData' in data:
+        chk_detail_data(data['detailData'], insti_key)
+    if 'additionData' in data:
+        chk_addition_data(data['additionData'], insti_key)
+
+    db.session.commit()
 
     return {
-               'status': 'success',
-               'message': 'Successfully updated.'
-           }, 201
+        'status': 'success',
+        'message': 'Successfully updated.'
+    }, 201
 
 
 def update_insti(data, insti_key):
-    if not insti_key: return
+    if not insti_key:
+        return
     insti = Insti.query.filter_by(insti_no=insti_key).first()
 
     for key, val in data.items():
-        insti.__setattr__(AcademyDto.academy._schema['properties'][key]['description'], val)
+        insti.__setattr__(
+            AcademyDto.academy._schema['properties'][key]['description'], val)
 
     insti.confirm_date = datetime.datetime.utcnow() if insti.confirm_yn == 1 else None
     insti.upd_id = Auth.get_logged_in_user(request)[0]['data']['name']
@@ -192,18 +211,22 @@ def chk_detail_data(data, insti_key):
 
     for o in data:
         # print(o)
-        exists = InstiDetail.query.filter_by(insti_no=insti_key, gbn=o['gbn'], code_no=o['codeNo']).first()
+        exists = InstiDetail.query.filter_by(
+            insti_no=insti_key, gbn=o['gbn'], code_no=o['codeNo']).first()
         if not exists:
             insert_detail_data(o, insti_key)
         else:
             update_detail_data(o, insti_key)
 
+    db.session.commit()
+
 
 def delete_detail_data(gbn, insti_key):
-    if not gbn or not insti_key: return
+    if not gbn or not insti_key:
+        return
 
-    db.session.query(InstiDetail).filter(InstiDetail.insti_no == insti_key
-                                                       , InstiDetail.gbn == gbn).delete()
+    db.session.query(InstiDetail).filter(InstiDetail.insti_no ==
+                                         insti_key, InstiDetail.gbn == gbn).delete()
     db.session.commit()
 
 
@@ -220,9 +243,12 @@ def chk_addition_data(data, insti_key):
         else:
             update_addition_data(o, insti_key)
 
+    db.session.commit()
+
 
 def insert_detail_data(data, insti_key):
-    if not insti_key: return
+    if not insti_key:
+        return
     new_insti_detail = InstiDetail(
         insti_no=insti_key,
         gbn=data['gbn'],
@@ -233,7 +259,8 @@ def insert_detail_data(data, insti_key):
 
 
 def insert_addition_data(data, insti_key):
-    if not insti_key: return
+    if not insti_key:
+        return
     new_insti_addition = InstiAddition(
         insti_no=insti_key,
         item_name=data['itemName'],
@@ -246,7 +273,8 @@ def insert_addition_data(data, insti_key):
 
 
 def update_addition_data(data, insti_key):
-    if not insti_key: return
+    if not insti_key:
+        return
     addition_data = InstiAddition.query.filter_by(insti_no=insti_key, item_name=data['itemName'],
                                                   item_value=data['itemValue']).first()
 
@@ -259,8 +287,10 @@ def update_addition_data(data, insti_key):
 
 
 def update_detail_data(data, insti_key):
-    if not insti_key: return
-    detail_data = InstiDetail.query.filter_by(insti_no=insti_key, gbn=data['gbn'], code_no=data['codeNo']).first()
+    if not insti_key:
+        return
+    detail_data = InstiDetail.query.filter_by(
+        insti_no=insti_key, gbn=data['gbn'], code_no=data['codeNo']).first()
 
     detail_data.insti_no = insti_key
     detail_data.gbn = data['gbn']
